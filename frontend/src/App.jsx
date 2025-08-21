92a9dc6c1fb791dab74a9a10216e5921aa77e83f
wkdwlgns12 <jjg530@kbu.ac.kr>
2025-08-21 09:34:33 +0900

    init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -112,6 +112,24 @@ function App() {
 
   }
 
+    const onUpdate = async (id, next) => {
+    try {
+      const current = Array.isArray(todos) ? todos.find(t => t._id == id) : null
+      if (!current) throw new Error("해당 ID의 todo가 없습니다.")
+
+      const { data } = await axios.put(`${API}/${id}`, next)
+
+      const updated = data?.updated ?? data?.todo ?? data;
+        setTodos(
+          prev => prev.map(t => (t._id === updated._id ? updated : t))
+        )
+
+    } catch (error) {
+      console.error("Todo update 실패",error)
+    }
+  }
+
+
   return (
     <div className='App'>
       <Header />
